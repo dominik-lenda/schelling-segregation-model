53ada83826bf52e225471a84b781fab9107b99d2
Dominik Lenda <dlenda1729@gmail.com>
2020-11-16 20:04:29 +0100

    Animate Schelling's model with gganimate.

--- schelling-segregation.R
+++ schelling-segregation.R
@@ -1,20 +1,21 @@
 rm(list=ls())
 # Schelling model is situated on a grid and in simple form one cell of a grid represents three states: uninhabited,
-# inhabited by a member of one group, inhabitd by a member of second group: 0, 1, 2.
+# inhabited by a member of one group, inhabitd by a member of second group: 0, 1, 2. This code presents simple Schelling model.
 
-number  <- 2000
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+
+
+# set up ------------------------------------------------------------------
 group  <- c(rep(0, 51*51-number), rep(1, number/2), rep(2, number/2))
 grid <- matrix(sample(group, 2601, replace = F), ncol = 51)
-
-par(mfrow=c(1, 2))
-image(grid, col=c("black", "red", "green"), axes = F)
-plot(runif(100, 0, 1), ylab = "percent happy",
-     xlab = "time", col = 'white', ylim = c(0, 1))
 
 alike_preference  <- .60
 
 happiness_tracker <- c()
 
+
+# function to navigate 8 cells around -------------------------------------
 get_neighbors<-function(coords) {
     n<-c()
     for (i in c(1:8)) {
@@ -76,7 +77,11 @@ get_neighbors<-function(coords) {
     n
 }
 
-for (t in c(1:1000)) {
+
+# Schelling's model -------------------------------------------------------
+matrices_list <- list()
+
+for (t in 1:100) {
     happy_cells <- c()
     unhappy_cells<-c()  
 
@@ -117,13 +122,13 @@ for (t in c(1:1000)) {
 
     happiness_tracker<-append(happiness_tracker,length(happy_cells)/(length(happy_cells) + length(unhappy_cells)))
 
-
+    # move unhappies to spaces that are not occupied
     rand<-sample(nrow(unhappy_cells))
     for (i in rand) {
         mover<-unhappy_cells[i,]
         mover_val<-grid[mover[1],mover[2]]
         move_to<-c(sample(1:51,1),sample(1:51,1))
-        move_to_val<-grid[move_to[1],move_to[2]]
+        move_to_val<-grid[move_to[1], move_to[2]]
         while (move_to_val > 0 ){
             move_to<-c(sample(1:51,1),sample(1:51,1))
             move_to_val<-grid[move_to[1],move_to[2]]
@@ -132,8 +137,30 @@ for (t in c(1:1000)) {
         grid[move_to[1],move_to[2]]<-mover_val
     }
 
-    par(mfrow=c(1,2))
-    image(grid,col=c("black","red","green"),axes=F)
-    plot(runif(100,0,1),ylab="percent happy",xlab="time",col="white",ylim=c(0,1))
-    lines(happiness_tracker,oma = c(0, 0, 2, 0),col="red")
+matrices_list[[t]] <- grid
 }
+
+
+# prepare data frame to ggplot2 -------------------------------------------
+df <- as.data.frame(do.call('rbind', matrices_list))
+df$id <- rep(1:51, 100)
+df$iteration <- rep(1:100, each = 51)
+
+require(reshape2)
+
+m_grid <- melt(df, id.var=c("id", "iteration"))
+
+m_grid
+
+
+# animate the plot with gganimate -----------------------------------------
+require(gganimate)
+p1 <- ggplot(m_grid, aes(x=variable,
+                         y=id,fill=factor(value))) +
+               geom_tile() 
+
+p2 <- p1 + transition_states(iteration,
+                  transition_length = 2,
+                  state_length = 1)
+
+p2
